1ae56cbba99c1666615b99206ce5606a2be7f100
Douglas Barbosa <douglassbarbosa22@gmail.com>
2020-12-13 17:11:33 -0300

    Projeto criado.

---
+++ src/main/java/com/douglassbarbosa/springboot/SprintbootstartApplication.java
@@ -0,0 +1,13 @@
+package com.douglassbarbosa.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintbootstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintbootstartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/douglassbarbosa/springboot/SprintbootstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douglassbarbosa.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintbootstartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
